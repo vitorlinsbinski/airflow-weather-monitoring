bc2c0c47e41e04ac1677ae0ae7cd1a5bb1282fbf
Vitor Linsbinski de Oliveira <vitor.linsbinski@hotmail.com>
2025-05-08 15:17:21 -0400

    refactor: dag modularization

--- dags/scripts/weather_etl.py
+++ dags/scripts/weather_etl.py
@@ -1,8 +1,10 @@
-from hook.openweather_hook import OpenWeatherHook
 import os
 import json
 import pytz
+
+from hook.openweather_hook import OpenWeatherHook
 from scripts.db_weather_operations import find_city_coordinates, create_new_city
+
 
 def extract_coordinates(city_name, state_code='MT', country_code='BR'):
     lat, lon = find_city_coordinates(city_name)
@@ -13,10 +15,9 @@ def extract_coordinates(city_name, state_code='MT', country_code='BR'):
     else:
         print('Getting coordinates from API')
         openweather_hook = OpenWeatherHook(city_name=city_name, state_code=state_code, country_code=country_code)
-
         coordinates_data = openweather_hook.get_coordinates_by_city()
 
-        if len(coordinates_data) == 0 or coordinates_data is None:
+        if not coordinates_data:
             return None, None
 
         lat = coordinates_data[0].get('lat')
@@ -24,33 +25,13 @@ def extract_coordinates(city_name, state_code='MT', country_code='BR'):
         country_code = coordinates_data[0].get('country')
 
         state_mapping = {
-            'Acre': 'AC',
-            'Alagoas': 'AL',
-            'Amapá': 'AP',
-            'Amazonas': 'AM',
-            'Bahia': 'BA',
-            'Ceará': 'CE',
-            'Distrito Federal': 'DF',
-            'Espírito Santo': 'ES',
-            'Goiás': 'GO',
-            'Maranhão': 'MA',
-            'Mato Grosso': 'MT',
-            'Mato Grosso do Sul': 'MS',
-            'Minas Gerais': 'MG',
-            'Pará': 'PA',
-            'Paraíba': 'PB',
-            'Paraná': 'PR',
-            'Pernambuco': 'PE',
-            'Piauí': 'PI',
-            'Rio de Janeiro': 'RJ',
-            'Rio Grande do Norte': 'RN',
-            'Rio Grande do Sul': 'RS',
-            'Rondônia': 'RO',
-            'Roraima': 'RR',
-            'Santa Catarina': 'SC',
-            'São Paulo': 'SP',
-            'Sergipe': 'SE',
-            'Tocantins': 'TO'
+            'Acre': 'AC', 'Alagoas': 'AL', 'Amapá': 'AP', 'Amazonas': 'AM', 'Bahia': 'BA',
+            'Ceará': 'CE', 'Distrito Federal': 'DF', 'Espírito Santo': 'ES', 'Goiás': 'GO',
+            'Maranhão': 'MA', 'Mato Grosso': 'MT', 'Mato Grosso do Sul': 'MS', 'Minas Gerais': 'MG',
+            'Pará': 'PA', 'Paraíba': 'PB', 'Paraná': 'PR', 'Pernambuco': 'PE', 'Piauí': 'PI',
+            'Rio de Janeiro': 'RJ', 'Rio Grande do Norte': 'RN', 'Rio Grande do Sul': 'RS',
+            'Rondônia': 'RO', 'Roraima': 'RR', 'Santa Catarina': 'SC', 'São Paulo': 'SP',
+            'Sergipe': 'SE', 'Tocantins': 'TO'
         }
 
         state = coordinates_data[0].get('state')
@@ -60,55 +41,61 @@ def extract_coordinates(city_name, state_code='MT', country_code='BR'):
 
     return lat, lon
 
-def create_directories(timestamp): 
-    base_path = './data/raw'
-    full_path = os.path.join(base_path, timestamp)
-    os.makedirs(base_path, exist_ok=True)
-    os.makedirs(full_path, exist_ok=True)
-    return full_path
 
-def load_cities(file_path='./data/raw/region/cities.json'):
-    with open(file_path, 'r') as file:
-        return json.load(file)  
-    
-def get_weather_data_for_city(city):
-    city_name = city.get('name')
-    state_code = city.get('state_code')
-    country_code = city.get('country_code')
-
-    lat, lon = extract_coordinates(city_name, state_code, country_code)
-
-    if lat is None or lon is None:
-        return None
-
-    openweather_hook = OpenWeatherHook(lat, lon)
-    return openweather_hook.get_current_weather_data()
-
-def save_weather_data(weather_data, full_path, timestamp):
-    filename = os.path.join(full_path, f'weather_{timestamp}.json')
-    with open(filename, 'w', encoding='utf-8') as f:
-        json.dump(weather_data, f, ensure_ascii=False, indent=4)
-    print(f'Dados salvos em {filename}')
-
-def extract_data(**context):
-    # Convertendo o horário UTC para horário local
+def create_timestamp_and_directories(**context):
     execution_date_utc = context['data_interval_end']
     cuiaba_tz = pytz.timezone('America/Cuiaba')
     execution_date_local = execution_date_utc.astimezone(cuiaba_tz)
     timestamp = execution_date_local.strftime('%Y%m%d_%H%M%S')
 
-    # Criando diretórios
-    full_path = create_directories(timestamp)
+    base_path = './data/raw'
+    full_path = os.path.join(base_path, timestamp)
+    os.makedirs(base_path, exist_ok=True)
+    os.makedirs(full_path, exist_ok=True)
 
-    # Carregando as cidades
-    cities = load_cities()
+    context['ti'].xcom_push(key='timestamp', value=timestamp)
+    context['ti'].xcom_push(key='full_path', value=full_path)
 
-    # Coletando os dados climáticos
+
+def load_cities(**context):
+    file_path = './data/raw/region/cities.json'
+    with open(file_path, 'r') as file:
+        cities = json.load(file)
+    context['ti'].xcom_push(key='cities', value=cities)
+
+
+def get_weather_data(**context):
+    ti = context['ti']
+    cities = ti.xcom_pull(key='cities', task_ids='load_cities')
     weather_data_cities = []
-    for city in cities:
-        weather_data_city = get_weather_data_for_city(city)
-        if weather_data_city is not None:
-            weather_data_cities.append(weather_data_city)
 
-    # Salvando os dados
-    save_weather_data(weather_data_cities, full_path, timestamp)
+    for city in cities:
+        city_name = city.get('name')
+        state_code = city.get('state_code')
+        country_code = city.get('country_code')
+
+        lat, lon = extract_coordinates(city_name, state_code, country_code)
+
+        if lat is None or lon is None:
+            continue
+
+        openweather_hook = OpenWeatherHook(lat, lon)
+        weather_data = openweather_hook.get_current_weather_data()
+
+        if weather_data:
+            weather_data_cities.append(weather_data)
+
+    ti.xcom_push(key='weather_data', value=weather_data_cities)
+
+
+def save_weather_data(**context):
+    ti = context['ti']
+    weather_data = ti.xcom_pull(key='weather_data', task_ids='get_weather_data')
+    full_path = ti.xcom_pull(key='full_path', task_ids='create_timestamp_and_directories')
+    timestamp = ti.xcom_pull(key='timestamp', task_ids='create_timestamp_and_directories')
+
+    filename = os.path.join(full_path, f'weather_{timestamp}.json')
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(weather_data, f, ensure_ascii=False, indent=4)
+
+    print(f'Dados salvos em {filename}')
--- dags/weather_dag.py
+++ dags/weather_dag.py
@@ -1,28 +1,59 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
-from scripts.weather_etl import extract_data
+from scripts.weather_etl import (
+    create_timestamp_and_directories,
+    load_cities,
+    get_weather_data,
+    save_weather_data
+)
 
 default_args = {
     'owner': 'vitorlinsbinski',
     'start_date': datetime.now() - timedelta(days=1),
-    'retires': 3,
+    'retries': 3,
     'retry_delay': timedelta(minutes=5),
 }
 
 dag = DAG(
     'weather_dag',
-    description='Capture current weather data from API',
+    description='ETL de clima com funções modulares',
     default_args=default_args,
     schedule_interval=timedelta(hours=1),
-    catchup=False
+    catchup=False,
 )
 
-extract_task = PythonOperator(
-    task_id = 'extract_weather_data',
-    python_callable=extract_data,
+# Tarefa 1: Criar timestamp e diretórios
+t1 = PythonOperator(
+    task_id='create_timestamp_and_directories',
+    python_callable=create_timestamp_and_directories,
     provide_context=True,
-    dag=dag,
+    dag=dag
 )
 
-extract_task
+# Tarefa 2: Carregar as cidades
+t2 = PythonOperator(
+    task_id='load_cities',
+    python_callable=load_cities,
+    provide_context=True,
+    dag=dag
+)
+
+# Tarefa 3: Obter dados climáticos
+t3 = PythonOperator(
+    task_id='get_weather_data',
+    python_callable=get_weather_data,
+    provide_context=True,
+    dag=dag
+)
+
+# Tarefa 4: Salvar dados climáticos
+t4 = PythonOperator(
+    task_id='save_weather_data',
+    python_callable=save_weather_data,
+    provide_context=True,
+    dag=dag
+)
+
+# Definindo a ordem das tarefas
+t1 >> t2 >> t3 >> t4
