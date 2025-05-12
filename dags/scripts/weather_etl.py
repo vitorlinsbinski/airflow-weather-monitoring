add36472d51f5fc1b950404772795055f13c3bf8
Vitor Linsbinski de Oliveira <vitor.linsbinski@hotmail.com>
2025-05-12 09:11:31 -0400

    refactor: separate dag in more tasks

--- dags/scripts/weather_etl.py
+++ dags/scripts/weather_etl.py
@@ -5,6 +5,19 @@ import pytz
 from hook.openweather_hook import OpenWeatherHook
 from scripts.db_weather_operations import find_city_coordinates, create_new_city
 
+import pandas as pd
+import numpy as np
+
+from datetime import time
+
+def format_date_to_utc(timestamp, format):
+    format_tz = pytz.timezone(format)
+    date_local = timestamp.astimezone(format_tz)
+    
+    timestamp_formated = date_local.strftime('%Y%m%d_%H%M%S')
+
+    return timestamp_formated
+
 
 def extract_coordinates(city_name, state_code='MT', country_code='BR'):
     lat, lon = find_city_coordinates(city_name)
@@ -44,9 +57,7 @@ def extract_coordinates(city_name, state_code='MT', country_code='BR'):
 
 def create_timestamp_and_directories(**context):
     execution_date_utc = context['data_interval_end']
-    cuiaba_tz = pytz.timezone('America/Cuiaba')
-    execution_date_local = execution_date_utc.astimezone(cuiaba_tz)
-    timestamp = execution_date_local.strftime('%Y%m%d_%H%M%S')
+    timestamp = format_date_to_utc(execution_date_utc, 'America/Cuiaba')
 
     base_path = './data/raw'
     full_path = os.path.join(base_path, timestamp)
@@ -57,7 +68,7 @@ def create_timestamp_and_directories(**context):
     context['ti'].xcom_push(key='full_path', value=full_path)
 
 
-def load_cities(**context):
+def read_cities_from_local(**context):
     file_path = './data/raw/region/cities.json'
     with open(file_path, 'r') as file:
         cities = json.load(file)
@@ -66,7 +77,7 @@ def load_cities(**context):
 
 def get_weather_data(**context):
     ti = context['ti']
-    cities = ti.xcom_pull(key='cities', task_ids='load_cities')
+    cities = ti.xcom_pull(key='cities', task_ids='read_cities_from_local')
     weather_data_cities = []
 
     for city in cities:
@@ -117,7 +128,7 @@ def get_weather_data(**context):
 
 def save_weather_data(**context):
     ti = context['ti']
-    weather_data = ti.xcom_pull(key='weather_data', task_ids='get_weather_data')
+    weather_data = ti.xcom_pull(key='weather_data', task_ids='get_weather_data_from_api')
     full_path = ti.xcom_pull(key='full_path', task_ids='create_timestamp_and_directories')
     timestamp = ti.xcom_pull(key='timestamp', task_ids='create_timestamp_and_directories')
 
@@ -126,3 +137,102 @@ def save_weather_data(**context):
         json.dump(weather_data, f, ensure_ascii=False, indent=4)
 
     print(f'Dados salvos em {filename}')
+    
+def read_raw_weather_data(**context):
+    execution_date_utc = context['data_interval_end']
+    timestamp = format_date_to_utc(execution_date_utc, 'America/Cuiaba')
+
+    base_path = './data/raw'
+    file_name = f'weather_{timestamp}.json'
+    target_path = os.path.join(base_path, timestamp, file_name)
+
+    with open(target_path, 'r', encoding='utf-8') as file:
+        weather_data = json.load(file)
+
+    context['ti'].xcom_push(key='weather_data', value=weather_data)
+    context['ti'].xcom_push(key='timestamp', value=timestamp)
+
+def normalize_weather_data(**context):
+    weather_data = context['ti'].xcom_pull(key='weather_data', task_ids='read_raw_weather_data')
+
+    weather_df = pd.json_normalize(
+        weather_data,
+        record_path=['weather'],
+        meta=[
+            'base', 'visibility', 'dt', 'timezone', 'name', 'cod',
+            ['coord', 'lon'], ['coord', 'lat'],
+            ['main', 'temp'], ['main', 'feels_like'], ['main', 'temp_min'], ['main', 'temp_max'],
+            ['main', 'pressure'], ['main', 'humidity'], ['main', 'sea_level'],
+            ['wind', 'speed'],
+            ['clouds', 'all'],
+            ['sys', 'sunrise'], ['sys', 'sunset']
+        ],
+        sep='_'
+    )
+
+    weather_df.drop(columns=['id', 'main', 'base', 'visibility'], inplace=True)
+
+    context['ti'].xcom_push(key='weather_df', value=weather_df.to_json())
+
+def cast_and_enrich_weather_data(**context):
+    weather_df = pd.read_json(context['ti'].xcom_pull(key='weather_df', task_ids='normalize_weather_data'))
+
+    weather_df = weather_df.astype({
+        'timezone': int,
+        'cod': int,
+        'coord_lon': np.float64,
+        'coord_lat': np.float64,
+        'main_temp': np.float64,
+        'main_temp_min': np.float64,
+        'main_temp_max': np.float64,
+        'main_pressure': int,
+        'main_humidity': int,
+        'main_sea_level': int,
+        'wind_speed': np.float32,
+        'clouds_all': int,
+        'sys_sunrise': int,
+        'sys_sunset': int
+    })
+
+    weather_df['datetime_utc'] = pd.to_datetime(weather_df['dt'], unit='s', utc=True)
+    weather_df['datetime_local'] = weather_df['datetime_utc'] + pd.to_timedelta(weather_df['timezone'], unit='s')
+    weather_df['datetime_local'] = weather_df['datetime_local'].dt.tz_localize(None)
+
+    weather_df['sys_sunrise'] = pd.to_datetime(weather_df['sys_sunrise'], unit='s') + pd.to_timedelta(weather_df['timezone'], unit='s')
+    weather_df['sys_sunrise'] = weather_df['sys_sunrise'].dt.strftime('%H:%M:%S')
+    weather_df['sys_sunrise'] = pd.to_datetime(weather_df['sys_sunrise'], format='%H:%M:%S').dt.time
+
+    weather_df['sys_sunset'] = pd.to_datetime(weather_df['sys_sunset'], unit='s') + pd.to_timedelta(weather_df['timezone'], unit='s')
+    weather_df['sys_sunset'] = weather_df['sys_sunset'].dt.strftime('%H:%M:%S')
+    weather_df['sys_sunset'] = pd.to_datetime(weather_df['sys_sunset'], format='%H:%M:%S').dt.time
+
+    weather_df['description'] = weather_df['description'].str.capitalize()
+
+    weather_df.drop(columns=['dt', 'timezone', 'datetime_utc'], inplace=True)
+
+    # Salva Parquet em um local temporário
+    base_path = './data/temp'
+    os.makedirs(base_path, exist_ok=True)
+    parquet_path = os.path.join(base_path, 'weather_df_enriched.parquet')
+    weather_df.to_parquet(parquet_path)
+
+    print(weather_df.info())
+
+    context['ti'].xcom_push(key='weather_df_parquet_path', value=parquet_path)
+
+def save_transformed_weather_data(**context):
+    timestamp = context['ti'].xcom_pull(key='timestamp', task_ids='read_raw_weather_data')
+    parquet_path = context['ti'].xcom_pull(key='weather_df_parquet_path', task_ids='cast_and_enrich_weather_data')
+
+    weather_df = pd.read_parquet(parquet_path)
+
+    print(weather_df.info())
+
+    base_path_transformed = './data/transformed'
+    full_path = os.path.join(base_path_transformed, timestamp)
+    os.makedirs(full_path, exist_ok=True)
+
+    file_name = f'weather_transformed_{timestamp}.csv'
+    outfile_path = os.path.join(full_path, file_name)
+
+    weather_df.to_csv(outfile_path, index=False)
--- dags/weather_dag.py
+++ dags/weather_dag.py
@@ -3,9 +3,13 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 from scripts.weather_etl import (
     create_timestamp_and_directories,
-    load_cities,
+    read_cities_from_local,
     get_weather_data,
-    save_weather_data
+    save_weather_data,
+    read_raw_weather_data,
+    normalize_weather_data,
+    cast_and_enrich_weather_data,
+    save_transformed_weather_data
 )
 
 default_args = {
@@ -33,15 +37,15 @@ t1 = PythonOperator(
 
 # Tarefa 2: Carregar as cidades
 t2 = PythonOperator(
-    task_id='load_cities',
-    python_callable=load_cities,
+    task_id='read_cities_from_local',
+    python_callable=read_cities_from_local,
     provide_context=True,
     dag=dag
 )
 
 # Tarefa 3: Obter dados climáticos
 t3 = PythonOperator(
-    task_id='get_weather_data',
+    task_id='get_weather_data_from_api',
     python_callable=get_weather_data,
     provide_context=True,
     dag=dag
@@ -49,11 +53,40 @@ t3 = PythonOperator(
 
 # Tarefa 4: Salvar dados climáticos
 t4 = PythonOperator(
-    task_id='save_weather_data',
+    task_id='save_raw_weather_data',
     python_callable=save_weather_data,
     provide_context=True,
     dag=dag
 )
 
+t5_1 = PythonOperator(
+    task_id='read_raw_weather_data',
+    python_callable=read_raw_weather_data,
+    provide_context=True,
+    dag=dag
+)
+
+t5_2 = PythonOperator(
+    task_id='normalize_weather_data',
+    python_callable=normalize_weather_data,
+    provide_context=True,
+    dag=dag
+)
+
+t5_3 = PythonOperator(
+    task_id='cast_and_enrich_weather_data',
+    python_callable=cast_and_enrich_weather_data,
+    provide_context=True,
+    dag=dag
+)
+
+t5_4 = PythonOperator(
+    task_id='save_transformed_weather_data',
+    python_callable=save_transformed_weather_data,
+    provide_context=True,
+    dag=dag
+)
+
 # Definindo a ordem das tarefas
-t1 >> t2 >> t3 >> t4
+t1 >> t2 >> t3 >> t4 >> t5_1 >> t5_2 >> t5_3 >> t5_4
+
